3a01d3ac1015bff8b1fa4c0f81c9aaeba73c801d
Ajsal sparky <130032226+A-J-S-A-L-S-P-A-R-K-Y@users.noreply.github.com>
2024-11-24 18:09:32 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
-const BOT_TOKEN = process.env.BOT_TOKEN || '';
-const OWNER = process.env.OWNER || '';
+const BOT_TOKEN = process.env.BOT_TOKEN || '7941895615:AAEDryG0cX7b7mugzBhNCr7OGf8etzA99yg';
+const OWNER = process.env.OWNER || 'Ajsal-Sparky';
 const OWNER_ID = process.env.OWNER_ID || 'https://t.me/belladesuda';
 const GROUP = process.env.GROUP || 'https://whatsapp.com/channel/0029Va9ZOf36rsR1Ym7O2x00';
 
