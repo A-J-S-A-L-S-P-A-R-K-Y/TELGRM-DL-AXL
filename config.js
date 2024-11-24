d8a1048e1f89e653fb3908b024ad4572199645ac
Ajsal sparky <130032226+A-J-S-A-L-S-P-A-R-K-Y@users.noreply.github.com>
2024-11-24 19:19:35 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 const BOT_TOKEN = process.env.BOT_TOKEN || '7941895615:AAEDryG0cX7b7mugzBhNCr7OGf8etzA99yg';
 const OWNER = process.env.OWNER || 'Ajsal-Sparky';
-const OWNER_ID = process.env.OWNER_ID || 'https://t.me/belladesuda';
-const GROUP = process.env.GROUP || 'https://whatsapp.com/channel/0029Va9ZOf36rsR1Ym7O2x00';
+const OWNER_ID = process.env.OWNER_ID || 'https://t.me/ajsalSparky';
+const GROUP = process.env.GROUP || 'https://whatsapp.com/channel/0029Va97vPqGU3BKjEyFQX3l';
 
 module.exports = {
   BOT_TOKEN,
